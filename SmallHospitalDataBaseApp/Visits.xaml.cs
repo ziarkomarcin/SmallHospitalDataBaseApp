295de144cdb78cd2beb2092b0bda05334c5dd0fa
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2021-02-23 21:43:51 +0100

    Visits.xaml.cs (NewUpdate)

--- SmallHospitalDataBaseApp/Visits.xaml.cs
+++ SmallHospitalDataBaseApp/Visits.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +16,7 @@ namespace SmallHospitalDataBaseApp
     /// </summary>
     public partial class Visits : Window
     {
+        VisitService VisitService = new VisitService();
         public Visits()
         {
             InitializeComponent();
@@ -31,17 +32,39 @@ namespace SmallHospitalDataBaseApp
             visitDataGrid.Columns[6].Visibility = Visibility.Collapsed;
         }
 
-        private void update_patient_button(object sender, RoutedEventArgs e)
+        private void add_visit_button(object sender, RoutedEventArgs e)
         {
-            // something
+            string datetime = visit_DateTextBox.Text;
+            DateTime.TryParseExact(datetime, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out DateTime date);
+
+
+            if (visit_doctorIDTextBox.Text != "" && visit_patientIDTextBox.Text != "" && visit_DateTextBox.Text != "")
+            {
+                string doct = visit_doctorIDTextBox.Text;
+                string pat = visit_patientIDTextBox.Text;
+                try
+                {
+                    int doctResult = Int32.Parse(doct);
+                    int patResult = Int32.Parse(pat);
+                    // Output: Unable to parse ''
+                    VisitService.AddVisit(doctResult, patResult, date);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show($@"Unable to parse '{doct}, {pat}'.
+They must be numbers!");
+                }
+
+            }
+            else MessageBox.Show("...Doctor ID, Visit ID & Date must be entered...");
+        }
+
+        private void delete_visit_button(object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void refresh_button(object sender, RoutedEventArgs e)
-        {
-            // something
-        }
-
-        private void delete_patient_button(object sender, RoutedEventArgs e)
         {
             // something
         }
@@ -55,5 +78,6 @@ namespace SmallHospitalDataBaseApp
         {
 
         }
+
     }
 }
