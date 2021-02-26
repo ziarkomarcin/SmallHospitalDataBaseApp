d7d3bff8a946407374699b730bf17d8816da31c8
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2021-02-27 00:50:43 +0100

    adding Assets, Classes & Properties

---
+++ Classes/DiagnosisClass.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallHospitalDataBaseApp
+{
+    class DiagnosisClass
+    {
+
+        public int id { get; set; }
+        public int vis { get; set; }
+        public string illness { get; set; }
+        public string desc { get; set; }
+
+        public DiagnosisClass(diagnosis diagnose)
+        {
+            id = diagnose.diagnosis_id;
+            vis = diagnose.visit_id;
+            illness = diagnose.illness_name;
+            desc = diagnose.description;
+        }
+    }
+    class DiagnosisService
+    {
+        MyHospitalDBEntities context = new MyHospitalDBEntities();
+
+        /// <summary>
+        /// List of Diagnosis
+        /// </summary>
+        /// <returns>List of Patients in DB</returns>
+        public List<diagnosis> GetList()
+        {
+            return context.diagnosis.ToList();
+        }
+
+        /// <summary>
+        /// Adding new Diagnosis in DB
+        /// </summary>
+        /// <param name="diagnosis_visit_id">Connect diagnosis with Visit</param>
+        /// <param name="diagnosis_illness_name">Patient's illness name</param>
+        /// <param name="diagnosis_description">Description of diagno & illness</param>\
+        public void AddDiagnosis(int diagnosis_visit_id, string diagnosis_illness_name, string diagnosis_description)
+        {
+            diagnosis Diagnosis = new diagnosis()
+            {
+                visit_id = diagnosis_visit_id,
+                illness_name = diagnosis_illness_name,
+                description = diagnosis_description,
+            };
+            context.diagnosis.Add(Diagnosis);
+            context.SaveChanges();
+        }
+
+        public void RemoveDiagnose(diagnosis diagnose)
+        {
+            if (diagnose != null) context.diagnosis.Remove(diagnose);
+            context.SaveChanges();
+
+        }
+
+
+        public diagnosis SelectDiagID(int diagnosisID)
+        {
+            var x = context.diagnosis.FirstOrDefault(y => y.diagnosis_id == diagnosisID);
+            return x;
+        }
+
+        public void EditDiagnosis(diagnosis diagnose, int diagnosis_visit_id, string diagnosis_illness_name, string diagnosis_description)
+        {
+            if (diagnose != null)
+            {
+                diagnose.visit_id = diagnosis_visit_id;
+                diagnose.illness_name = diagnosis_illness_name;
+                diagnose.description = diagnosis_description;
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Classes/DoctorsClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallHospitalDataBaseApp
+{
+
+
+    public class DoctorsClass
+    {
+        public int id { get; set; }
+        public string doctors_name { get; set; }
+        public string doctors_surname { get; set; }
+        public DateTime employment { get; set; }
+        public DateTime release { get; set; }
+
+       public DoctorsClass(doctors doctor)
+        {
+            id = doctor.doctor_id;
+            doctors_name = doctor.firstname;
+            doctors_surname = doctor.lastname;
+            employment = doctor.date_of_employment;
+            release = (DateTime)doctor.date_of_release;
+        }
+    }
+
+    public class DoctorsService
+    {
+        MyHospitalDBEntities context = new MyHospitalDBEntities();
+
+        /// <summary>
+        /// List of Doctors
+        /// </summary>
+        /// <returns>List of Doctors in DB</returns>
+        public List<doctors> GetList()
+        {
+            return context.doctors.ToList();
+        }
+
+        /// <summary>
+        /// Adding new Doctor in DB
+        /// </summary>
+        /// <param name="doctors_name">Doctor's firstname</param>
+        /// <param name="doctors_lastname">Doctor's lastname </param>
+        /// <param name="doctors_employment_date">Date of Doctor's employment</param>
+        ///// <param name="doctors_release_date">Date of Doctor's release</param>
+        public void AddDoctor(string doctors_name, string doctors_lastname, DateTime doctors_employment_date/*, DateTime doctors_release_date*/)
+        {
+            doctors Doctor = new doctors()
+            {
+                firstname = doctors_name,
+                lastname = doctors_lastname,
+                date_of_employment = doctors_employment_date,
+                //date_of_release = doctors_release_date,
+            };
+            context.doctors.Add(Doctor);
+            context.SaveChanges();
+        }
+
+        /// <param name="doctors_release_date">Date of Doctor's release</param>
+        public void EditDoctor(doctors doctor, string doctor_name, string doctor_surname, DateTime employment, DateTime release)
+        {
+            if (doctor != null)
+            {
+                doctor.firstname = doctor_name;
+                doctor.lastname = doctor_surname;
+                doctor.date_of_employment = employment;
+                doctor.date_of_release = release;
+            }
+            context.SaveChanges();
+        }
+
+        public void RemoveDoctor(doctors doctor)
+        {
+            if (doctor != null) context.doctors.Remove(doctor);
+            context.SaveChanges();
+        }
+
+        public doctors SelectDoctorID(int doctorID)
+        {
+            var x = context.doctors.FirstOrDefault(y => y.doctor_id == doctorID);
+            return x;
+        }
+
+    }
+}
