7f92befff5214ea0614abccc3730230415d95ca4
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2021-02-26 23:41:29 +0100

    add

---
+++ SQLQuery_INSERT_INTO.sql
@@ -0,0 +1,26 @@
+﻿INSERT INTO doctors VALUES ('Danuta', 'Motowidło', '2020-02-01', '2020-05-01'); 
+INSERT INTO doctors VALUES ('Marianna', 'Put', '2020-04-03', null); 
+INSERT INTO doctors VALUES ('Beata', 'Kozidrak', '2019-02-10', null); 
+INSERT INTO doctors VALUES ('Magdalena', 'Gessler', '2018-04-03', null); 
+
+INSERT INTO patients VALUES ('Filip', 'Kryszak', '2020-10-17', '2020-10-18'); 
+INSERT INTO patients VALUES ('Anna', 'Lewandowska', '2013-01-01', null); 
+INSERT INTO patients VALUES ('Wanda', 'Tyralik', '2018-06-04', '2020-07-01');
+INSERT INTO patients VALUES ('Jan', 'Kowalski', '2018-06-04', '2020-07-01');
+
+INSERT INTO visits VALUES (1, 1, '2018-06-04'); 
+INSERT INTO visits VALUES (2, 2, '2013-01-01'); 
+INSERT INTO visits VALUES (3, 3, '2020-07-01'); 
+INSERT INTO visits VALUES (4, 4, '2020-10-17'); 
+
+INSERT INTO diagnosis VALUES (1, 'renal colic', 'severe pain'); 
+INSERT INTO diagnosis VALUES (2, 'shingles', 'rash and pain'); 
+INSERT INTO diagnosis VALUES (3, 'tapeworm', 'weakness, stomach pain, vomiting'); 
+INSERT INTO diagnosis VALUES (4, 'covid-19', 'fever, muscle pain'); 
+/*
+INSERT INTO treatments VALUES ('renal colic treatment', 1, '2018-06-04'); 
+INSERT INTO treatments VALUES ('shingles treatment', 2, '2013-01-01'); 
+INSERT INTO treatments VALUES ('tapeworm treatment', 3, '2020-07-01'); 
+INSERT INTO treatments VALUES ('covid-19 treatment', 4, '2019-06-17'); 
+*/
+
