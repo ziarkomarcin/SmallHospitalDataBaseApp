01f7c36a29a97fef708770d5e2b07a5f4efb56c9
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2021-02-27 00:52:33 +0100

    adding more files

---
+++ Diagnosis.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SmallHospitalDataBaseApp
+{
+    /// <summary>
+    /// Interaction logic for Diagnosis.xaml
+    /// </summary>
+    public partial class Diagnosis : Window
+    {
+        DiagnosisService DiagnosisService = new DiagnosisService();
+
+        public Diagnosis()
+        {
+            InitializeComponent();
+            DiagnosisService diagnosis = new DiagnosisService();
+
+            this.diagnosisDataGrid.ItemsSource = diagnosis.GetList();
+        }
+
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //for (int index = 0; index < 5; index++)
+            //{
+            //    patientDataGrid.Columns[index].Visibility = Visibility.Visible;
+            //}
+            diagnosisDataGrid.Columns[0].Header = "Diagnosis ID";
+            diagnosisDataGrid.Columns[1].Header = "Visit ID";
+            diagnosisDataGrid.Columns[2].Header = "Illness Name";
+            diagnosisDataGrid.Columns[3].Header = "Description";
+
+            //for (int indexI = 0; indexI > 5; indexI++)
+            //{
+            //    patientDataGrid.Columns[indexI].Visibility = Visibility.Collapsed;
+
+            //}
+            diagnosisDataGrid.Columns[4].Visibility = Visibility.Collapsed;
+            diagnosisDataGrid.Columns[5].Visibility = Visibility.Collapsed;
+        }
+
+        private void del_diagnosis_button(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var diagnosis = DiagnosisService.SelectDiagID(diagnosisID);
+                DiagnosisService.RemoveDiagnose(diagnosis);
+            }
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("Cannot remove that diagnosis.");
+            }
+            Refresh();
+        }
+
+        private void cancel_button(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private int diagnosisID = 0;
+        private void diagnosisDataGrid_Selection(object sender, SelectionChangedEventArgs e)
+        {
+            diagnosis d = this.diagnosisDataGrid.SelectedItem as diagnosis;
+            if (d is null)
+            {
+                this.diagnosis_IDTextBox.Text = string.Empty.ToString();
+                this.diagnosis_VisitIDTextBox_Copy.Text = string.Empty.ToString();
+                this.diagnosis_IllnessNameTextBox_Copy.Text = string.Empty;
+                this.diagnosis_DescriptionTextBox_Copy.Text = string.Empty;
+
+                return;
+            }
+            this.diagnosis_VisitIDTextBox_Copy.Text = d.visit_id.ToString();
+            this.diagnosis_IllnessNameTextBox_Copy.Text = d.illness_name;
+            this.diagnosis_DescriptionTextBox_Copy.Text = d.description;
+            var txt = this.diagnosisID = d.diagnosis_id;
+            this.diagnosis_IDTextBox.Text = txt.ToString();
+        }
+
+        private void add_diagnosis_button(object sender, RoutedEventArgs e)
+        {
+            bool flag = false;
+            if (diagnosis_VisitIDTextBox.Text != "" && diagnosis_IllnessNameTextBox.Text != "" && diagnosis_DescriptionTextBox.Text != "")
+            {
+                string input = diagnosis_VisitIDTextBox.Text;
+                try
+                {
+                    int result = Int32.Parse(input);
+                    // Output: Unable to parse ''
+                    DiagnosisService.AddDiagnosis(result,
+                                                diagnosis_IllnessNameTextBox.Text,
+                                                diagnosis_DescriptionTextBox.Text);
+                    flag = true;
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show($@"Unable to parse '{input}'.
+It must be a number!");
+                    this.diagnosis_VisitIDTextBox.Text = string.Empty;
+                }
+                
+            }
+            else MessageBox.Show("...Visit ID, Illness Name & Description must be entered...");
+            
+            if (flag == true)
+            {
+                Refresh();
+            }
+        }
+
+        private void Refresh()
+        {
+            Diagnosis newWindow = new Diagnosis();
+            Application.Current.MainWindow = newWindow;
+            newWindow.Show();
+            this.Close();
+        }
+
+        private void edit_diagnosis_button(object sender, RoutedEventArgs e)
+        {
+            if (diagnosis_VisitIDTextBox_Copy.Text != "" && diagnosis_IllnessNameTextBox_Copy.Text != "" && diagnosis_DescriptionTextBox_Copy.Text != "")
+            {
+
+                var diagnose = DiagnosisService.SelectDiagID(diagnosisID);
+
+                string doct = diagnosis_VisitIDTextBox_Copy.Text.ToString().Trim();
+                int a;
+                bool doctRes = Int32.TryParse(diagnosis_VisitIDTextBox_Copy.Text.Trim(), out a);
+                string b = diagnosis_IllnessNameTextBox_Copy.Text.ToString().Trim();
+                if (!doctRes)
+                {
+                    MessageBox.Show("ID must be number");
+                    return;
+                }
+                
+                DiagnosisService.EditDiagnosis(diagnose, a, b, diagnosis_DescriptionTextBox_Copy.Text);
+                Refresh();
+            }
+            else MessageBox.Show("All fields must be filled");
+
+        }
+    }
+}
---
+++ Visits.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace SmallHospitalDataBaseApp
+{
+    /// <summary>
+    /// Interaction logic for Visits.xaml
+    /// </summary>
+    public partial class Visits : Window
+    {
+        VisitService VisitService = new VisitService();
+
+        public Visits()
+        {
+            InitializeComponent();
+            VisitService visit = new VisitService();
+
+            this.visitDataGrid.ItemsSource = visit.GetList();
+        }
+
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            visitDataGrid.Columns[4].Visibility = Visibility.Collapsed;
+            visitDataGrid.Columns[5].Visibility = Visibility.Collapsed;
+            visitDataGrid.Columns[6].Visibility = Visibility.Collapsed;
+
+            visitDataGrid.Columns[0].Header = "Visit ID";
+            visitDataGrid.Columns[1].Header = "Doctor ID";
+            visitDataGrid.Columns[2].Header = "Patient ID";
+            visitDataGrid.Columns[3].Header = "Visit Date";
+
+        }
+
+        private void add_visit_button(object sender, RoutedEventArgs e)
+        {
+            string datetime = visit_DateTextBox.Text;
+            DateTime.TryParseExact(datetime, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out DateTime date);
+
+            bool flag = false;
+            if (visit_doctorIDTextBox.Text != "" && visit_patientIDTextBox.Text != "" && visit_DateTextBox.Text != "")
+            {
+                string doct = visit_doctorIDTextBox.Text;
+                string pat = visit_patientIDTextBox.Text;
+                try
+                {
+                    int doctResult = Int32.Parse(doct);
+                    int patResult = Int32.Parse(pat);
+                    // Output: Unable to parse ''
+                    VisitService.AddVisit(doctResult, patResult, date);
+
+                    flag = true;
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show($@"Unable to parse '{doct}, {pat}'.
+They must be numbers!");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("...Doctor ID, Visit ID & Date must be entered...");
+                this.visit_IDTextBox.Text = string.Empty;
+            }
+            if (flag == true)
+            {
+                Refresh();
+            }
+
+        }
+
+        private void delete_visit_button(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var visit = VisitService.SelectVisitID(visitID);
+                VisitService.RemoveVisit(visit);
+            }
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("Cannot remove that visit.");
+            }
+            Refresh();
+        }
+
+            private void Refresh()
+        {
+            Visits newWindow = new Visits();
+            Application.Current.MainWindow = newWindow;
+            newWindow.Show();
+            this.Close();
+        }
+      
+        private void cancel_button(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private int visitID = 0;
+        private void visitDataGrid_Selection(object sender, SelectionChangedEventArgs e)
+        {
+            visits v = this.visitDataGrid.SelectedItem as visits;
+            if (v is null)
+            {
+                this.visit_doctorIDTextBox2.Text = string.Empty.ToString();
+                this.visit_patientIDTextBox2.Text = string.Empty.ToString();
+                this.visit_DateTextBox2.Text = string.Empty;
+                return;
+            }
+            this.visit_doctorIDTextBox2.Text = v.doctor_id.ToString();
+            this.visit_patientIDTextBox2.Text = v.patient_id.ToString();
+            this.visit_DateTextBox2.Text = v.date_of_visit.ToString();
+            var txt = this.visitID = v.visit_id;
+            this.visit_IDTextBox.Text = txt.ToString();
+        }
+
+        private void edit_visit_button(object sender, RoutedEventArgs e)
+        {
+            if (visit_doctorIDTextBox2.Text != "" && visit_patientIDTextBox2.Text != "" && visit_DateTextBox2.Text != "")
+            {
+                string date = visit_DateTextBox2.Text;
+                DateTime.TryParseExact(date, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out DateTime newDate);
+
+                var visit = VisitService.SelectVisitID(visitID);
+
+                string doct = visit_doctorIDTextBox2.Text.Trim();
+                int a;
+                bool doctRes = Int32.TryParse(visit_doctorIDTextBox2.Text.Trim(), out a);
+                if (!doctRes)
+                {
+                    MessageBox.Show("Doctor ID must be number");
+                    return;
+                }
+                string pat = visit_patientIDTextBox2.Text.Trim();
+                int b;
+                bool patRes = Int32.TryParse(visit_patientIDTextBox2.Text.Trim(), out b);
+                if (!patRes)
+                {
+                    MessageBox.Show("Patient ID must be number");
+                    return;
+                }
+                if (a < 0)
+                {
+                    MessageBox.Show("ID cannot be less than 0.");
+                    return;
+                }
+                if (b < 0)
+                {
+                    MessageBox.Show("ID cannot be less than 0.");
+                    return;
+                }
+                VisitService.EditVisit(visit, a, b, newDate);
+                Refresh();
+            }
+            else MessageBox.Show("All fields must be filled");
+        }
+
+    }
+}
